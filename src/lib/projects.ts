15aac5848fd4ae777a4c7d0573dde3fa00487fa1
chizor iwuh <thisischizor@gmail.com>
2024-06-21 16:37:27 +0100

    projects

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,31 +1,69 @@
-export type Project = {
-	id: string
-	name: string
-	description?: string
-	stage: ProjectStage
-	status: ProjectStatus
-	favorite: boolean
-	tags: string[]
-	links: Links
-  image?: string
-}
+import { Project, ProjectStage, ProjectStatus } from "./types"
 
-export enum ProjectStatus {
-	Active = "active",
-	Maintenance = "maintenance",
-	Inactive = "inactive",
-}
-
-export enum ProjectStage {
-	Development = "development",
-	Production = "production",
-}
-
-export type Links = {
-	github?: string
-	website?: string
-	additionalLinks?: {
-		link: string
-		title?: string
-	}[]
-}
+export const projects: Project[] = [
+	{
+		id: "valet",
+		name: "Valet",
+		description: "A social login in-built wallet for solana dapps.",
+		favorite: true,
+		links: {
+			github: "https://github.com/thewuhxyz/valet",
+			website: "https://valetw.xyz",
+			additionalLinks: [
+				{ link: "https://app.valetw.xyz", title: "Valet App" },
+				{ link: "https://portal.valetw.xyz", title: "Valet Portal" },
+				{ link: "https://demo.valetw.xyz", title: "Valet Demo" },
+			],
+		},
+		stage: ProjectStage.Development,
+		status: ProjectStatus.Inactive,
+		tags: ["sveltekit", "anchor", "solana", "express"],
+		imageUrl: "",
+	},
+	{
+		id: "merstab",
+		name: "Merstab",
+		description: "Liquidity Protocol for CLOB DEX.",
+		favorite: true,
+		links: {
+			github: "https://github.com/merstab"
+		},
+		stage: ProjectStage.Development,
+		status: ProjectStatus.Inactive,
+		tags: ["defi", "solana", "ws"],
+		imageUrl: "",
+	},
+	{
+		id: "zeta-liqiudeo",
+		name: "Zeta Liquideo",
+		description: "Real-time WS market data API for Zeta Markets. NPM package",
+		favorite: false,
+		links: {
+			github: "https://github.com/merstab/zeta-liquideo",
+			additionalLinks: [
+				{ link: "https://npmjs.com/package/zeta-liquideo", title: "npm" },
+			],
+		},
+		stage: ProjectStage.Production,
+		status: ProjectStatus.Inactive,
+		tags: ["zeta", "solana", "ws", "defi"],
+		imageUrl: "",
+	},
+	{
+		id: "01-flask",
+		name: "01 flask",
+		description: "Real-time WS market data API for 01 exchange. NPM package",
+		favorite: false,
+		links: {
+			github: "https://github.com/merstab/01-flask",
+			additionalLinks: [
+				{ link: "https://npmjs.com/package/01-flask", title: "npm" },
+			],
+		},
+		stage: ProjectStage.Production,
+		status: ProjectStatus.Inactive,
+		tags: [],
+		imageUrl: "",
+	},
+	
+]
